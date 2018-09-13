dee38d4634ab68007a8cbd29cf50b1da1d71e7fc
ol4ash <shaukavets@gmail.com>
2018-09-14 00:41:56 +0300

    task 2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  var x = 0;
+  var y = 0;
+  var z = 0;
+
+  if (preferences.length == 0) {finalcount = 0}
+  	else {
+
+	for (var n=0; n<=preferences.length-1; n++) {
+
+	var kx = preferences[n];
+	var ky = preferences[kx-1];
+	var kz = preferences[ky-1];
+
+	if ((kz == n+1)&&(kz != (ky))) {
+		count++;
+	}
+
+	finalcount = count/3; // because each triangle will be counted 3 times abc, bca, cab
+}
+}
+
+	return finalcount;
 };
